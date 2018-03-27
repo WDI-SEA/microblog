203e69bfca7961e19921e315d62b3b2bc721cf55
JackPound <jackpound@live.com>
2018-03-27 16:44:23 -0700

    some form functionality, form submissions pushed to an array. need to assign array strings (posts) to html elements for viewing on page.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const inputForm = document.getElementById("input-form");
-  inputForm.addEventListener("submit", function(e){
+const submitForm = document.getElementById("submit-form");
+let posts = [];
+ 
+submitForm.addEventListener("submit", function(e){
     e.preventDefault();
+    const blogPost = document.getElementById('input-box').value;
+    console.log(blogPost);
+    // let post = document.getElementById('post');
+    // post.innerHTML = blogPost
+    posts.unshift(blogPost)
+    console.log(posts)
   });
+
+
+
