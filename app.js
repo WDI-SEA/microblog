ea9142094d24f4115b5fb5c1bf2725bd5b0ed388
JackPound <jackpound@live.com>
2018-03-27 16:56:20 -0700

    working JS to convert input to an html <p> element and pushing down older submissions. sloppy sloppy code though

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const submitForm = document.getElementById("submit-form");
 let posts = [];
- 
+let post1 = document.getElementById('post0')
+let post2 = document.getElementById('post1')
+let post3 = document.getElementById('post2')
+let post4 = document.getElementById('post3')
+
+
 submitForm.addEventListener("submit", function(e){
     e.preventDefault();
     const blogPost = document.getElementById('input-box').value;
@@ -9,7 +14,9 @@ submitForm.addEventListener("submit", function(e){
     // post.innerHTML = blogPost
     posts.unshift(blogPost)
     console.log(posts)
+    post1.innerHTML = posts[0]
+    post2.innerHTML = posts[1]
+    post3.innerHTML = posts[2]
+    post4.innerHTML = posts[3]
   });
-
-
 
