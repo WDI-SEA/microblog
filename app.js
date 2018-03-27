14bd64be746d2a403188cb9c4694f121cf0c7618
bradserlis <bradserlis@yahoo.com>
2018-03-27 16:15:20 -0700

    2nd commit - add to ul list without array

---
+++ app.js
@@ -0,0 +1,23 @@
+const blogEntryForm = document.getElementById("blogEntryForm");
+const blogHome = document.getElementById("blogHome");
+const submitButton = document.querySelector("button");
+
+blogEntryForm.addEventListener("submit", function(e){
+	e.preventDefault();
+	
+
+}
+
+
+// 	let newBlogEntry = document.getElementById("blogEntryField").value;
+// 	let newContent = document.createTextNode(newBlogEntry);
+// 	let newLi = document.createElement('LI');
+// 	newLi.appendChild(newContent);
+// 	document.getElementById("blogHome").appendChild(newLi);
+// });
+
+
+	// let textBase = document.createElement('li');
+	// let textEntry = document.createTextNode(e.value);
+	// textBase.appendChild('textEntry');
+	// document.getElementById('blogHome').appendChild('textEntry');
