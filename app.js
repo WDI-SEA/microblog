ed8e1f99e77a0556969e6a396092910374c9a226
graemeerickson <graemeerickson@gmail.com>
2018-03-27 18:00:16 -0700

    Working code to receive new post entry and insert into entry list

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const updateEntries = () => {
-   console.log('hello');
-   let entryList = document.getElementsByTagName('ul');
-   console.log(entrylist);
+const updateEntries = function(e) {
+   e.preventDefault();
+   
+   let latestEntry = document.getElementsByTagName('input')[0];
+   let latestEntryText = latestEntry.value;
+   
+   let newListEntry = document.createElement('li');
+   newListEntry.innerHTML = latestEntryText;
+   document.getElementById('entry-list').appendChild(newListEntry);
 }
 
 document.getElementById('submit-button').addEventListener('click',updateEntries);
