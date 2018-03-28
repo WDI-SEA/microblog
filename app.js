e0f18bedef9cf126d014a1807001bf156cb6ba5d
smithalex24 <smith25243@gmail.com>
2018-03-27 17:52:04 -0700

    Started JS

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var prompts = [
-'What have you got to say?'
+console.log("sanity check");
 
-];
+const blogPost = ["Blog One", "Blog two", "Blog Three"]
+
+
+
+const addPost = function () {
+	let getInput = document.getElementById("type").value;
+	console.log(getInput);
+	let newItem = document.createElement('li');
+	console.log(newItem);
+	newItem.innerHTML = getInput;
+	document.getElementById("list").appendChild(newItem);
+
+}
+
+
+document.getElementById("Post").addEventListener("click", addPost);
+
+
