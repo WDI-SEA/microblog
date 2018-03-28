e12641aebb44f81a5e108d0d9c0d4d38a1d055be
cippero <gilwein@gmail.com>
2018-03-27 21:45:57 -0700

    improved style and js, added reset and counter

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const userSubmit = document.getElementById("submitButton");
+const userDelete = document.getElementById("deleteButton");
+const form = document.getElementById("myForm");
 const list = document.getElementById("postList");
+let postCount = document.getElementById("postsAmount");
 let userInputArr = [];
 
 userSubmit.addEventListener("click", (e) => {
 	e.preventDefault();
 	let userInput = document.getElementById("inputMessage").value;
-	let addList = document.createElement("li");
-	addList.classList.add("messageBubble");
-	addList.innerHTML = userInput;
-	list.appendChild(addList);
-	userInputArr.push(userInput);
-	userInput = null;
+	if (userInput) {
+		let addList = document.createElement("li");
+		addList.classList.add("messageBubble");
+		addList.innerHTML = userInput;
+		list.appendChild(addList);
+		userInputArr.push(userInput);
+		postsAmount.innerHTML++;
+	}
+	form.reset();
 	console.log(userInputArr);
+	console.log(postsAmount);
+})
+
+userDelete.addEventListener("click", (e) => {
+	e.preventDefault();
+	while(list.firstChild){
+		list.removeChild(list.firstChild);
+	}
+	userInputArr = [];
+	postsAmount.innerHTML = 0;
 })
 
