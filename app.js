eb7378a00fcce591024d121a966418647cac477d
graemeerickson <graemeerickson@gmail.com>
2018-03-28 09:05:58 -0700

    Implemented array and 'insertBefore' to insert new post to top of the list rather than bottom of list

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+let entriesArr = [];
+
 const updateEntries = function(e) {
    e.preventDefault();
    
    let latestEntry = document.getElementsByTagName('input')[0];
    let latestEntryText = latestEntry.value;
+   entriesArr.unshift(latestEntryText);
    
    let newListEntry = document.createElement('li');
-   newListEntry.innerHTML = latestEntryText;
-   document.getElementById('entry-list').appendChild(newListEntry);
+   newListEntry.innerHTML = entriesArr[0];
+   
+   let list = document.getElementById('entry-list');
+   list.insertBefore(newListEntry,list.childNodes[0]);
+   document.getElementById('entry-form').reset();
 }
 
 document.getElementById('submit-button').addEventListener('click',updateEntries);
