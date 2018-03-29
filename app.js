b211d8eb20adaf73202d243406772ecd72586306
thegreatsantini <lss13@my.fsu.edu>
2018-03-28 20:46:56 -0700

    todo list functionality

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const btn = document.querySelector('#post');
 const input = document.querySelector('input')
+const newLi = document.querySelector('#todoList')
+const card = document.querySelector('.chip')
+const trashCan = document.querySelector('i')
 
 let post = []
 
 const handleChange = (e) => {
-    console.log(e.target.value)
+    const newTodo = e.target.value;
+    const newItem = document.createElement('li')
+    newItem.innerHTML = newTodo
+    newLi.appendChild(newItem)
+    
 }
 
-const handleSubmit = (e) => {
-    console.log(e.value)
+const handleHover = (e) => {
+    trashCan.classList.toggle('fa')
+    trashCan.classList.toggle('fa-trash')
+  e.preventDefault();
+    
+    
 }
 
+const handleDelete = () => {
+    console.log('delete me')
+}
+
+const handleCompleted = () => {
+    newLi.classList.toggle('completed')
+}
+
+
+trashCan.addEventListener('click', handleDelete)
+card.addEventListener('mouseover', handleHover)
 input.addEventListener('change', handleChange)
-btn.addEventListener('submit', handleSubmit)
+newLi.addEventListener('click', handleCompleted)
