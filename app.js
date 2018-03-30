8e534be20518d8c1626afa29391907954b24ae1b
ortizk <ortizk@live.com>
2018-03-29 17:44:35 -0700

    localstorage kept on ul on refresh

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const postForm = document.getElementById("post-form");
 let things = []
 
+const insertOneElement = (postInput) => {
+	let newPost = document.createElement("li");
+			newPost.innerHTML = postInput.value;
+			console.log(newPost);
+			document.getElementById("postedHtml").appendChild(newPost);
+
+}
+
+const populateOnRefresh = (temp) => {
+		for(let i = 0; i < temp.length; i++) {
+			let newPost = document.createElement("li");
+			newPost.innerHTML = temp[i];
+			console.log(newPost);
+			document.getElementById("postedHtml").appendChild(newPost);
+
+		};
+};
+
 postForm.addEventListener("submit", function(e){
 	e.preventDefault();
 	let postInput = document.getElementById("post-input");
@@ -8,14 +26,12 @@ postForm.addEventListener("submit", function(e){
 
 	//consider making line 10-14 a function that can be called later when adding things to li
 	//so what I should have is a function with a string perameter(postInput.value(which would be a string by nature))
-	let newPost = document.createElement("li");
-	newPost.innerHTML = postInput.value;
-	console.log(newPost);
-	document.getElementById("postedHtml").appendChild(newPost);
-
+	
 	addToLocalStorage(postInput.value);
+	insertOneElement(postInput);
 	postInput.value = "";
 });
+
 
 
 //for local storage, I want to make an empty array. make 
@@ -33,11 +49,53 @@ const addToLocalStorage = (item) => {
 const getFromLocalStorage = () => {
 	console.log("getting from local storage");
 	things = JSON.parse(localStorage.getItem("lSThings")) || [];
+	// for(let i = 0; i < things.length; i++) {
+	// 	let newPost = document.createElement("li");
+	// 	newPost.innerHTML = postInput[i];
+	// 	console.log(newPost);
+	// 	postedHtml.appendChild(newPost);
+	// };
 //we parse to turn the fake string back into a real array because, again, we can only store strings in localStorage
 	console.log(things, typeof things);
 
 	//make items from things array
+	populateOnRefresh(things);
 };
 
 getFromLocalStorage();
+
+
+
+// const populateOnRefresh = (temp) => {
+// 	let newPost = document.createElement("li");
+// 		newPost.innerHTML = postInput.value;
+// 		console.log(newPost);
+// 		document.getElementById("postedHtml").appendChild(newPost);
+// };
+
+// populateOnRefresh(things);
+
+// const decapitateMyChild = () => {
+// 	while(things[0])
+// 		things.removechild;
+// };
+
+// decapitateMyChild();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
