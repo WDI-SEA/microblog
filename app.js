528e1b707f158ef9212af98a65fa0bfa7352ef2f
graemeerickson <graemeerickson@gmail.com>
2018-03-29 19:15:01 -0700

    Implemented localStorage for getting & setting - not yet removing

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let entriesArr = [];
+let storedPosts;
 var list;
 // let closeButtonsArr = [];
 
+const setLocalStorage = (entriesArr) => {
+   console.log("Setting localStorage with array:",entriesArr);
+   console.log("localStorage - before: ",storedPosts);
+   localStorage.setItem('storedPosts', JSON.stringify(entriesArr));
+   console.log("localStorage - after: ",storedPosts);
+}
+
 const deleteEntry = function(e) {
    let clickedCloseButton = this.parentElement;
-   console.log(clickedCloseButton);
    list.removeChild(clickedCloseButton);
 }
 
@@ -22,11 +29,29 @@ const updateEntries = function(e) {
    list.insertBefore(newListEntry,list.childNodes[0]);
    document.getElementById('entry-form').reset();
    
+   setLocalStorage(entriesArr);
+
    let closeButtonsArr = document.getElementsByClassName('remove-item');
-   console.log(closeButtonsArr);
    for (let i = 0; i < closeButtonsArr.length; i++) {
       closeButtonsArr[i].addEventListener('click',deleteEntry);
    }
 }
 
+const getLocalStorage = () => {
+   let savedPostsStr = localStorage.getItem('storedPosts');
+   console.log(savedPostsStr);
+   if (savedPostsStr !== null) {
+      entriesArr = JSON.parse(savedPostsStr);
+
+      list = document.getElementById('entry-list');
+      
+      entriesArr.forEach( (item, index) => {
+         let newListEntry = document.createElement('li');
+         newListEntry.innerHTML = item + "<span class='remove-item'>x</span>";
+         list.appendChild(newListEntry);
+      });
+   };
+}
+
+getLocalStorage();
 document.getElementById('submit-button').addEventListener('click',updateEntries);
