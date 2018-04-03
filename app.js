9c95dc994750ef4efd89bbb076685dd172851700
jessiheger <jhmheger@gmail.com>
2018-04-02 18:37:51 -0700

    add local storage and remove item capability

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 const blogForm = document.getElementById("blogEntry");
-const blogArray = [];
-const list = document.getElementById("blogList");
+let blogArray = [];
+let list = document.getElementById("blogList");
 
-blogForm.addEventListener("submit", function(e){
-    e.preventDefault();
-let getBlogEntry = document.getElementById("link").value
-let newLi = document.createElement("li")
+$(document).ready(function(){
+	getFromLocalStorage();
 
-newLi.innerHTML = getBlogEntry;
-blogArray.push(newLi);
-let lastEntry = (blogArray[blogArray.length-1]);
-list.appendChild(lastEntry);
-})
+	blogForm.addEventListener("submit", function(e){
+    	e.preventDefault();
+    	let valueTypedIn = document.getElementById("link").value
+    	blogArray.push(valueTypedIn);
+		createListItem(valueTypedIn);
+		sendToLocalStorage();
+		blogEntry.reset();
+	})
+});
+
+function createListItem (textToAdd) {	
+	let newLi = document.createElement("li");
+	newLi.innerHTML = textToAdd;
+	newLi.addEventListener('click', function(){
+		var index = blogArray.indexOf(this.textContent);
+			console.log('index of', this.textContent, 'is', index);
+		// splice blogArray to remove the item clicked
+		blogArray.splice(index, 1);
+			console.log(blogArray);
+		// send blogarray to local storage
+		sendToLocalStorage();
+		// remove list item using this.remove
+		this.remove(index);
+	})
+	list.appendChild(newLi);
+}
+
+function sendToLocalStorage () {
+	localStorage.setItem('storedBlogs', JSON.stringify(blogArray));
+}
+
+function getFromLocalStorage () {
+	blogArray = JSON.parse(localStorage.getItem('storedBlogs')) || [];
+	console.log('loaded from LS', blogArray);
+	blogArray.forEach(function(item){
+		createListItem(item);
+	});
+}
